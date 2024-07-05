3f5b7648e5e5fa9ca096b688d12a008848989d09
Nicolò Manvilli <n.manvilli@gmail.com>
2024-07-05 11:37:13 +0200

    Fix isElementInViewport behaviuour on partialliVisible

--- animation-utilities.js
+++ animation-utilities.js
@@ -45,8 +45,12 @@ function isElementInViewport(el, partiallyVisible = false) {
     const { top, left, bottom, right } = el.getBoundingClientRect();
     const { innerHeight, innerWidth } = window;
     if (partiallyVisible) {
-       return ( (top >= 0 && top <= innerHeight) || (bottom >= 0 && bottom <= innerHeight) ) &&
-            ( (left >= 0 && left <= innerWidth) || (right >= 0 && right <= innerWidth) );
+       return (
+		(top >= 0 && top <= innerHeight) || (bottom >= 0 && bottom <= innerHeight) || (top < 0 && bottom > innerHeight)
+	) &&
+	(
+		(left >= 0 && left <= innerWidth) || (right >= 0 && right <= innerWidth) || (left < 0 && right > innerWidth)
+	);
     }  
     return top >= 0 && left >= 0 && bottom <= innerHeight && right <= innerWidth;
 }
